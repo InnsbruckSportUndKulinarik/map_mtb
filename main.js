69eec157df38e41c97039ec4dad8d563881af2a7
Tashi Freyer <tashi.freyer@student.uibk.ac.at>
2023-06-19 13:54:23 +0200

    added funtion for hütten; downloaded icon

--- main.js
+++ main.js
@@ -41,3 +41,39 @@ L.control.rainviewer({
     animationInterval: 500,
     opacity: 0.5
 }).addTo(map);
+
+// Hütten 
+function writeHuettenLayer(jsondata) {
+    L.geoJSON(jsondata, {
+        pointToLayer: function (feature, latlng) {
+            return L.marker(latlng, {
+                icon: L.icon({
+                    iconUrl: "icons/restaurant.png",
+                    iconAnchor: [16, 37],
+                    popupAnchor: [0, -37]
+                })
+            });
+        },
+        onEachFeature: function (feature, layer) {
+            let prop = feature.properties;
+            let mas = feature.geometry.coordinates[2]
+            let date_time = new Date(prop.date);
+            let WG;
+            if (prop.WG) {
+                WG = (prop.WG).toFixed(1);
+            } else {
+                WG = "-";
+            }
+            //let WG = (prop.WG)?(prop.WG*3.6).toFixed(1):"-";
+            layer.bindPopup(`
+            <h1>${prop.name}, ${mas} m ü.A. </h1><ul>
+                <li>Lufttemperatur (Grad °): ${prop.LT || "-"} </li>
+                <li>relative Luftfeuchte (%): ${prop.RH || "-"} </li>
+                <li>Windgeschwindigkeit (km/h): ${WG}</li>
+                <li>Schneehöhe (cm): ${prop.HS || "-"}</li>
+            </ul></>
+            <span>${date_time.toLocaleString()}</span>
+        `);
+        }
+    }).addTo(themaLayer.stations)
+}
