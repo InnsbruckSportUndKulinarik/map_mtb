9f436a0d5f5d8f98c1107c2127ac8785d14a9a21
Tashi Freyer <tashi.freyer@student.uibk.ac.at>
2023-06-19 14:01:57 +0200

    function for huetten added

--- main.js
+++ main.js
@@ -56,24 +56,14 @@ function writeHuettenLayer(jsondata) {
         },
         onEachFeature: function (feature, layer) {
             let prop = feature.properties;
-            let mas = feature.geometry.coordinates[2]
-            let date_time = new Date(prop.date);
-            let WG;
-            if (prop.WG) {
-                WG = (prop.WG).toFixed(1);
-            } else {
-                WG = "-";
-            }
-            //let WG = (prop.WG)?(prop.WG*3.6).toFixed(1):"-";
             layer.bindPopup(`
-            <h1>${prop.name}, ${mas} m ü.A. </h1><ul>
-                <li>Lufttemperatur (Grad °): ${prop.LT || "-"} </li>
-                <li>relative Luftfeuchte (%): ${prop.RH || "-"} </li>
-                <li>Windgeschwindigkeit (km/h): ${WG}</li>
-                <li>Schneehöhe (cm): ${prop.HS || "-"}</li>
+            <h1>${prop.name}, ${ele|| " "} m ü.A. </h1><ul>
+                <li>Öffnungszeiten: ${prop.opening_ho || "-"} </li>
+                <li>Website: ${prop.contact_we || "-"} </li>
+                <li>Telefon: ${prop.phone|| "-"}</li>
+                <li>Email: ${prop.email || "-"}</li>
             </ul></>
-            <span>${date_time.toLocaleString()}</span>
         `);
         }
-    }).addTo(themaLayer.stations)
+    }).addTo(themaLayer.huetten)
 }
