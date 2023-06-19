0f6e40a6fefb73b8414f4bc073d349dd8b33a3da
Tashi Freyer <tashi.freyer@student.uibk.ac.at>
2023-06-19 13:16:03 +0200

    layercontrol, scale and rainviewer plugin

--- main.js
+++ main.js
@@ -15,3 +15,29 @@ let themaLayer = {
     stops: L.featureGroup(),
     huetten: L.featureGroup()
 }
+
+// WMTS und Leaflet TileLayerProvider Hintergrundlayer
+let layercontrol = L.control.layers({"Esri WorldTopoMap": L.tileLayer.provider("Esri.WorldTopoMap") }, {
+    "Mountainbikerouten": themaLayer.routen,
+    "ÖPNV-Haltestellen": themaLayer.stops,
+    "Hütten": themaLayer.huetten
+}).addTo(map)
+
+layerControl.expand()
+
+// Maßstab
+L.control.scale({
+    imperial: false,
+}).addTo(map);
+
+// Rainviewer Plugin
+L.control.rainviewer({ 
+    position: 'bottomleft',
+    nextButtonText: '>',
+    playStopButtonText: 'Play/Stop',
+    prevButtonText: '<',
+    positionSliderLabelText: "Hour:",
+    opacitySliderLabelText: "Opacity:",
+    animationInterval: 500,
+    opacity: 0.5
+}).addTo(map);
